46026007c5ca5efe62924dd735bbd73053404451
EQUASHNZRKUL <justinkae@gmail.com>
2018-06-19 12:28:47 -0700

    fwdalg can take optional t and qF parameters now

--- HMM.py
+++ HMM.py
@@ -29,97 +29,108 @@ class Graph(object):
         self.start = data[0][0]
         self.final = data[-1][0]
 
-    # --- Setters & Getters ---
-        """ Gets root of the graph """
-        def getStart(self):
-            return self.start
+# --- Setters & Getters ---
+    """ Gets root of the graph """
+    def getStart(self):
+        return self.start
 
-        """ Gets end node of the graph """
-        def getFinal(self):
-            return self.final
-        
-        """ Sets new root of the graph 
-        Pre-conditions:
-        newStart: is an orphan node that exists already in the graph """
-        def setStart(self, newStart):
-            self.start = newStart
+    """ Gets end node of the graph """
+    def getFinal(self):
+        return self.final
+    
+    """ Sets new root of the graph 
+    Pre-conditions:
+    newStart: is an orphan node that exists already in the graph """
+    def setStart(self, newStart):
+        self.start = newStart
 
-        """ Sets new end node of the graph 
-        Pre-conditions:
-        newFinal: is an orphan node that exists already in the graph """
-        def setFinal(self, newFinal):
-            self.final = newFinal
+    """ Sets new end node of the graph 
+    Pre-conditions:
+    newFinal: is an orphan node that exists already in the graph """
+    def setFinal(self, newFinal):
+        self.final = newFinal
 
-        """ Returns the size of the graph """
-        def size(self):
-            return len(self.Q)
+    """ Returns the size of the graph """
+    def size(self):
+        return len(self.Q)
 
-        """ Returns the list of nodes of the graph"""
-        def list(self):
-            return self.Q
-        
-        """ Returns the [i]th element of the graph
-        Pre-conditions
-        [i]: i < self.size()"""
-        def elt(self, i):
-            return self.Q[i]
-        
-        """ Gets A of the graph """
-        def getA(self):
-            return self.A
+    """ Returns the list of nodes of the graph"""
+    def list(self):
+        return self.Q
+    
+    """ Returns the [i]th element of the graph
+    Pre-conditions
+    [i]: i < self.size()"""
+    def elt(self, i):
+        return self.Q[i]
+    
+    """ Gets A of the graph """
+    def getA(self):
+        return self.A
 
-        """ Gets B of the graph """
-        def getB(self):
-            return self.B
-        
-        """ Sets new A of the graph 
+    """ Gets B of the graph """
+    def getB(self):
+        return self.B
+    
+    """ Sets new A of the graph 
         Pre-conditions:
         newA: is an orphan node that exists already in the graph """
-        def setA(self, newA):
-            self.A = newA
+    def setA(self, newA):
+        self.A = newA
 
-        """ Sets new B of the graph 
+    """ Sets new B of the graph 
         Pre-conditions:
         newB: is an orphan node that exists already in the graph """
-        def setB(self, newB):
-            self.B = newB
+    def setB(self, newB):
+        self.B = newB
+    
+    """ Returns the index of [node] in the graph, returns -1 if [node] doesn't
+        exist in the graph
+        Pre-conditions: [node] is a string """
+    def index(self, node):
+        try:
+            return self.Q.index(node)
+        except ValueError:
+            print("%s was not found in graph's Q set" %node)
+            return -1
         
-    # --- Real methods ---
-        """ Returns the weight between nodes [parent] and [child]. 
+    
+# --- Real methods ---
+    """ Returns the weight between nodes [parent] and [child]. 
         Pre-conditions: 
         parent: is a node in the graph. 
         child: is a node in the graph. There must be a direct connection from 
             [parent] to [child]. """
-        def a(self, parent, child, ints=False):
-            if ints:
-                i, j = parent, child
-            else: 
-                try:
-                    i = self.Q.index(parent)
-                    j = self.Q.index(child)
-                except ValueError:
-                    print("%s or %s were not found in graph's Q set" %(parent, child))
-                    raise ValueError()
-            return self.A[i][j]
+    def a(self, parent, child, ints=False):
+        if ints:
+            i, j = parent, child
+        else: 
+            try:
+                i = self.Q.index(parent)
+                j = self.Q.index(child)
+            except ValueError:
+                print("%s or %s were not found in graph's Q set" %(parent, child))
+                raise ValueError()
+        return self.A[i][j]
 
-        """ Returns the likelihood of the given [observation]
-        Pre-conditions:
-        state: is a node in the graph
-        observation: is an observation recorded by the node. """
-        def b(self, state, observation, ints=False):
-            if ints:
-                i = state
-            else:
-                try:
-                    i = self.Q.index(state)
-                except ValueError:
-                    print("%s was not found in the graph" %state)
-                    raise ValueError()
-            obList = self.B[i]
-            for k, v in obList:
-                if observation == k:
-                    return v
-            return 0
+    """ Returns the likelihood of the given [observation]
+    Pre-conditions:
+    state: is a node in the graph
+    observation: is an observation recorded by the node. """
+    def b(self, state, observation, ints=False):
+        if ints:
+            i = state
+        else:
+            try:
+                i = self.Q.index(state)
+            except ValueError:
+                print("%s was not found in the graph" %state)
+                raise ValueError()
+        obList = self.B[i]
+        for k, v in obList:
+            if observation == k:
+                return v
+        return 0
 
 
 """Executes Forward Algorithm on graph object graph
@@ -127,24 +138,28 @@ class Graph(object):
     obList: set of all possible observations from every node of [graph]
     graph: graph of N states representing a L-R HMM (each path has a weight
         representing a probability & all paths travel in one direction.
-    returns the probability of observing the observation sequence [obList] in [graph]"""
-def FwdAlg(obList, graph):
+    returns the probability of observing the observation sequence [obList] in [graph]
+    alpha(t, q) where q is implicitly the final state, and t is the length of [obList]"""
+def FwdAlg(obList, graph, final=None, T=None):
+    final = final if final != None else graph.getFinal()
+    T = T if T != None else len(obList)
     # Initialize
     fwd = [[0.0 for z in range(graph.size())] for z in range(len(obList))]
     for i in range(graph.size()):
         s = graph.elt(i)
         fwd[0][i] = graph.a(graph.getStart(), s) * graph.b(s,obList[0])
     # Iteration
-    for dt in range(len(obList)-1):
+    for dt in range(T-1):
         t = dt + 1
         for i in range(graph.size()):
             s = graph.elt(i)
             evalStep = lambda x : fwd[t-1][x] * graph.a(graph.elt(x), s) * graph.b(s, obList[t])
             fwd[t][i] = sum(map(evalStep, range(graph.size())))
     # Termination
-    termStep = lambda s : fwd[len(obList)-1][s] * graph.a(graph.elt(s), graph.getFinal())
+    termStep = lambda s : fwd[T-1][s] * graph.a(graph.elt(s), graph.getFinal())
+    fwd[T-1][-1] = sum(map(termStep, range(graph.size())))
     print(fwd)
-    return sum(map(termStep, range(graph.size())))
+    return fwd[T-1][graph.index(final)]
 
 
 """ Executes Viterbi Algorithm on a HMM graph object
@@ -231,6 +246,7 @@ def xi(t, i, j, obList, graph):
     b = graph.b(j, t, True)
     fullalpha = FwdAlg(obList, graph)
     return (alph * a * b * beta)/fullalpha
+
 
 # #TODO: Implement Fwd-Bwd Algo 
 # def forward_backward(obList, state set = )
