65233acd6f9e9be3881656284971c059fa076d49
EQUASHNZRKUL <justinkae@gmail.com>
2018-06-21 15:05:45 -0700

    testing valid_lines

--- Filefinder.ml
+++ Filefinder.ml
@@ -1,3 +1,8 @@
+open Str
+open Sys
+
+(** [read_file filename] is the string list of the text found in [filename] each
+  * elt being a different line.*)
 let read_file filename = 
   let lines = ref [] in
   let chan = open_in filename in
@@ -17,45 +22,62 @@ let list_of_files foldername =
 
 (** [accesstext_voxforge folder] is the access function for VoxForge prompts. It
   * returns the text representation of the data found in location [folder]. *)
-let accesstext_voxforge user = 
-  let dest = String.concat "" [user; "/etc/prompts-original"] in
+let accesstext_voxforge data = 
+  let dest = String.concat "" [data; "/etc/prompts-original"] in
   read_file dest
 
 (** [accesswav_voxforge user files] returns the list of wave destinations of the
-  * files named [files] in the [user] folder. *)
-let accesswav_voxforge user files = 
-  let f x = String.concat "" [user; "/wav/"; x; ".wav"] in
+  * files named [files] in the [user] folder for VoxForge data. *)
+let accesswav_voxforge data files = 
+  let f x = String.concat "" [data; "/wav/"; x; ".wav"] in
   List.map f files
 
-(** [valid_lines] returns the valid wave file names from the [prompt_list] which
-  * is the list of prompts *)
+(** [contains s1 s2] is true if s2 exists in s1 as a substring. 
+  * (s1 contains s2) == (s1 <-= s2) *)
+let (<-=) s1 s2 = 
+  let re = Str.regexp_string s2 in
+  try ignore (Str.search_forward re s1 0); true
+  with Not_found -> false
 
-let rec valid_lines 
+(** [valid_lines] returns the valid wave file names from the [prompt_list] which
+  * is the list of prompts, each element corresponding to a separate 5 sec wav
+  * recording, that contain a word from [cmdlist]. If they do exist, they are
+  * put into a list and returned in a tuple with their wav title. *)
+let rec valid_lines prompt_list cmdlist = 
+  let f acc prompt = 
+    let filtered = List.filter (fun c -> prompt <-= c) cmdlist in
+    if filtered != [] then (*command found in prompt, find wavid and cmd list*)
+      let i = String.index prompt ' ' in
+      (String.sub prompt 0 i, filtered) :: acc
+    else acc in
+  List.fold_left f [] prompt_list
+
+
   
-(** [find_words' wordlist text audio filelist acc] is the helper function to 
+(** [find_words' cmdlist text audio filelist acc] is the helper function to 
   * [find_words] and handles the recursive section. Returns a (wav * prompt)
-  * list where the prompts contain an instance of any word from [wordlist] and 
+  * list where the prompts contain an instance of any word from [cmdlist] and 
   * the wavs are audio representations of each prompt. The [text] and [audio] 
   * functions are dir -> prompt and dir -> wav location respectively. [acc] is 
   * the return list so far.*)
-let rec find_words' wordlist text audio filelist acc = 
+(* let rec find_words' cmdlist text audio filelist acc = 
   match filelist with
   | [] -> acc
   | h::t -> 
     let prompt = text h in
     
     let acc' = in 
-    findwords' wordlist accessfunc t acc'
+    findwords' cmdlist accessfunc t acc' *)
 
-(** [find_words wordlist text audio foldername] is the (wav * prompt) list 
+(** [find_words cmdlist text audio foldername] is the (wav * prompt) list 
   * of data points in dataset [foldername] with prompt access_function of [text]
   * and wav location access_function of [audio] that contain an instance of any 
-  * word found in [wordlist]. *)
-let find_words wordlist text audio foldername = 
+  * word found in [cmdlist]. *)
+(* let find_words cmdlist text audio foldername = 
   let filelist = list_of_files foldername in
     let f acc file = 
       let prompt = text h in
 
-  List.fold_left fold_func [] filelist 
+  List.fold_left fold_func [] filelist  *)
 
-  (* find_words' wordlist text audio filelist [] *)
+  (* find_words' cmdlist text audio filelist [] *)
