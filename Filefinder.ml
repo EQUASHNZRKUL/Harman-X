cf7fc44a78e9b6275b9ac60a7eb813b8dc7b9853
EQUASHNZRKUL <justinkae@gmail.com>
2018-06-21 16:24:55 -0700

    done find_words

--- Filefinder.ml
+++ Filefinder.ml
@@ -28,9 +28,7 @@ let accesstext_voxforge data =
 
 (** [accesswav_voxforge user files] returns the list of wave destinations of the
   * files named [files] in the [user] folder for VoxForge data. *)
-let accesswav_voxforge data files = 
-  let f x = String.concat "" [data; "/wav/"; x; ".wav"] in
-  List.map f files
+let accesswav_voxforge data wav = String.concat "" [data; "/wav/"; wav; ".wav"]
 
 (** [contains s1 s2] is true if s2 exists in s1 as a substring. 
   * (s1 contains s2) == (s1 <-= s2) *)
@@ -43,44 +41,23 @@ let (<-=) s1 s2 =
   * is the list of prompts, each element corresponding to a separate 5 sec wav
   * recording, that contain a word from [cmdlist]. If they do exist, they are
   * put into a list and returned in a tuple with their wav title. *)
-let rec valid_lines prompt_list cmdlist = 
+let rec valid_lines prompt_list cmdlist audiofile acc = 
   let f acc prompt = 
     let filtered = List.filter (fun c -> prompt <-= c) cmdlist in
     if filtered != [] then (*command found in prompt, find wavid and cmd list*)
       let i = String.index prompt ' ' in
-      (String.sub prompt 0 i, filtered) :: acc
+      (audiofile (String.sub prompt 0 i), filtered) :: acc
     else acc in
-  List.fold_left f [] prompt_list
-
-
-  
-(** [find_words' cmdlist text audio filelist acc] is the helper function to 
-  * [find_words] and handles the recursive section. Returns a (wav * prompt)
-  * list where the prompts contain an instance of any word from [cmdlist] and 
-  * the wavs are audio representations of each prompt. The [text] and [audio] 
-  * functions are dir -> prompt and dir -> wav location respectively. [acc] is 
-  * the return list so far.*)
-  (*TODO: Probably can be done with a fold instead*)
-(* let rec find_words' cmdlist text audio filelist acc = 
-  match filelist with
-  | [] -> acc
-  | h::t -> 
-    let prompt = text h in
-    
-    let acc' = in 
-    findwords' cmdlist accessfunc t acc' *)
+  List.fold_left f acc prompt_list
 
 (** [find_words cmdlist text audio foldername] is the (wav * prompt) list 
   * of data points in dataset [foldername] with prompt access_function of [text]
   * and wav location access_function of [audio] that contain an instance of any 
   * word found in [cmdlist]. *)
-let find_words cmdlist text audio foldername = 
-  let filelist = list_of_files foldername in
-  let validList = valid_lines filelist cmdlist in
-
-    (* let f acc file = 
-      let prompt = text h in *)
-
-  List.fold_left fold_func [] filelist 
-
+let find_words cmdlist text audio dataset = 
+  let filelist = list_of_files dataset in
+    let f acc file = (*fold function to acc & process each file in the dataset*)
+      let promptlist = text file in
+      valid_lines promptlist cmdlist (audio file) acc in
+    List.fold_left f [] filelist
   (* find_words' cmdlist text audio filelist [] *)
