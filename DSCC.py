9f5ec963398816c56df31f9ebdf2ee36ae053ea2
EQUASHNZRKUL <justinkae@gmail.com>
2018-06-27 16:42:32 -0700

    mfcc -> dscc framework established

---
+++ DSCC.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import numpy
+from python_speech_features import sigproc
+from python_speech_features import base
+from scipy.fftpack import dct
+
+def dscc(signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13,
+         nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97, 
+         ceplifter=22, appendEnergy=True, winfunc=lambda x:numpy.ones((x,))):
+    feats, energies = base.fbank(signal, samplerate, winlen, winstep, nfilt, nfft, 
+                            lowfreq, highfreq, preemph, winfunc)
+    # OBTAIN DELTA
+    # GAUUSIANIZATION NONLINEARITY
+    feats = numpy.log(feats)
+    feats = dct(feats, type=2, axis=1, norm='ortho')[:,:numcep]
+    feats = base.lifter(feats,ceplifter)
+    if appendEnergy: feats[:,0] = numpy.log(energies) # replace first cepstral coefficient with log of frame energy
+    feats = base.delta(feats, 2) #verify if 2 is right
+    return feats
