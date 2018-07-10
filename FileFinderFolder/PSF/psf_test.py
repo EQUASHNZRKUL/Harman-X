a8862f38e175a5a30d8bd3356b6ae4130904fbf4
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-10 13:50:56 -0700

    implemented psf_test

--- FileFinderFolder/PSF/psf_test.py
+++ FileFinderFolder/PSF/psf_test.py
@@ -2,19 +2,23 @@ from python_speech_features import mfcc
 from python_speech_features import delta
 from python_speech_features import logfbank
 
+from numpy import savez 
+
 import scipy.io.wavfile as wav
 
-def read_res(dir):
-    resfile = open(dir, 'r')
+def read_res(filename, d={}):
+    resfile = open(filename, 'r')
     key = None
-    d = {}
     for line in resfile:
         if "[" in line :
-            key = line[1:-4]
-        if (not "[" in line) or (not "]" in line):
+            key = line[1:-5]
+            d[key] = []
+        if (not "[" in line) and (not "]" in line):
             dir = line.strip()[:-1]
+            print dir
             mfcc = get_mfcc(dir)
-            d[key] = d[key].append(mfcc)
+            d[key] = d[key] + [mfcc]
+    return d
 
 def get_mfcc(wavfile):
     (rate, sig) = wav.read (wavfile)
@@ -23,8 +27,14 @@ def get_mfcc(wavfile):
     fbank_feat = logfbank(sig,rate)
     return d_mfcc_feat
 
-def main():
+def write_dict(d):
+    for cmd, arrlist in d.iteritems():
+        savez("./MFCCData/"+cmd, arrlist)
 
+def main():
+    dic = {}
+    read_res("/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/surf_results.txt", dic)
+    write_dict(dic)
 
 if __name__ == "__main__":
     main()
