6a42e447ef784ed787877b830ab826fa51bdf831
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-20 08:57:06 -0700

    main clean-up

--- FileFinderFolder/main.ml
+++ FileFinderFolder/main.ml
@@ -8,15 +8,21 @@ let main () =
   let args = Sys.argv in
   let cmdlist = getCmdList argv.(1) [] in
   let dirpath = if argv.(2) = "" then "./FileFinderData" else argv.(2) in
-  if argv.(2) = "export" then 
+
+let main () = 
+  let simpleton = fun x y z -> x in
+  let args = Sys.argv in
+  let cmdlist = getCmdList argv.(2) [] in
+  let dirpath = if argv.(3) = "" then "./FileFinderData" else argv.(2) in
+  if argv.(3) = "export" then 
     let results = total_res_dict "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/" in
     let trash = dir_accumulate results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data/" in
     let trash = dir_accumulate_merged results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data_merged/" cmdlist in
     ignore(trash)
   else
   (* let taccess = accesstext_maker args.(3) args.(4) in *)
-  let waccess = accesswav_maker args.(3) args.(5) in
-  let res,txtout = (match argv.(6) with 
+  let waccess = accesswav_maker args.(4) args.(6) in
+  let res,txtout = (match argv.(1) with 
     | "vox" -> (find_words cmdlist accesstext_vox accesswav_vox dirpath true, 
                 "vox_results.txt")
     | "libri" -> (find_words cmdlist accesstext_libri accessflac_libri dirpath 
@@ -29,12 +35,12 @@ let main () =
     | _ -> (D.empty,"")) in
   let cmd_dict = make_cmd_dict res D.empty in
   let oc = open_out ("results/" ^ txtout) in
-  if argv.(6) = "ami" then 
+  if argv.(1) = "ami" then 
     (ignore(print_result_ami oc (ami_dict dirpath cmdlist));
     close_out oc;
     let oc = open_out ("results/ami_results.txt") in
     ami_textify "results/metadata.ami_results.txt" oc)
-  else if argv.(6) = "surf" then 
+  else if argv.(1) = "surf" then 
     ignore (print_result oc res)
   else ignore (print_result oc cmd_dict);
   close_out oc;
