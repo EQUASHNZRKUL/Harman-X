39d1b51b947f341ccd71dbb6dc5f2ea33fa024ce
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-23 09:45:46 -0700

    merged main

--- FileFinderFolder/main.ml
+++ FileFinderFolder/main.ml
@@ -8,13 +8,42 @@ let main () =
   let args = Sys.argv in
   let cmdlist = getCmdList argv.(1) [] in
   let dirpath = if argv.(2) = "" then "./FileFinderData" else argv.(2) in
+  match args.(1) with
+  | "export" ->     
+    let results = total_res_dict "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/" in
+    let trash = dir_accumulate results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data/" in
+    let trash = dir_accumulate_merged results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data_merged/" cmdlist in
+    ignore(trash)
+  | "ami" -> 
+    let oc = open_out ("results/metadata.ami_results.txt")
+    (ignore(print_result_ami oc (ami_dict dirpath cmdlist));
+    close_out oc;
+    let oc = open_out ("results/ami_results.txt") in
+    ami_textify "results/metadata.ami_results.txt" oc)
+  | "surf" -> 
+    let res, txtout = (surf_dict dirpath cmdlist, "surf_results.txt") in
+    ignore (print_result oc res)
+  | "vox" | "libri" | "vy" | "wsj" -> 
+    let res, txtout = (match argv.(1) with
+    | "vox" -> (find_words cmdlist accesstext_vox accesswav_vox dirpath true, 
+        "vox_results.txt")
+    | "libri" -> (find_words cmdlist accesstext_libri accessflac_libri dirpath 
+        true, "libri_results.txt")
+    | "vy" -> (find_words cmdlist accesstext_vy accesswav_vy dirpath false, 
+      "vy_results.txt")
+    | "wsj" -> print_endline "wsj"; (wsj0_dict dirpath cmdlist, "wsj_results.txt")) in
+    let cmd_dict = make_cmd_dict res D.empty in
+    let oc = open_out ("results/" ^ txtout) in
+    print_result oc cmd_dict
+  | _ -> failwith (Invalid_argument "argv 1 is invalid")
+  ;;
 
-let main () = 
+(* let main () = 
   let simpleton = fun x y z -> x in
   let args = Sys.argv in
   let cmdlist = getCmdList argv.(2) [] in
   let dirpath = if argv.(3) = "" then "./FileFinderData" else argv.(2) in
-  if argv.(3) = "export" then 
+  if argv.(1) = "export" then 
     let results = total_res_dict "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/" in
     let trash = dir_accumulate results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data/" in
     let trash = dir_accumulate_merged results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data_merged/" cmdlist in
@@ -49,6 +78,6 @@ let main () =
   (* wsj0_unbox "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/FileFinderData/WSJ0_meta/wsjdt/wsj0" *)
   (* flatten "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/FileFinderData/LibriSpeech_360/train-clean-360" *)
   (* unflatten "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/FileFinderData/Vystidial/data/" *)
-  ;;
+  ;; *)
 
 main ()
