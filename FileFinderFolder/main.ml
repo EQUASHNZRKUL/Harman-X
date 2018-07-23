73c7f14f3e62b43eed114e4e5e2bc278fc3963cf
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-23 10:10:05 -0700

    cleaned up main

--- FileFinderFolder/main.ml
+++ FileFinderFolder/main.ml
@@ -6,8 +6,8 @@ open Data
 let main () = 
   let simpleton = fun x y z -> x in
   let args = Sys.argv in
-  let cmdlist = getCmdList argv.(1) [] in
-  let dirpath = if argv.(2) = "" then "./FileFinderData" else argv.(2) in
+  let cmdlist = getCmdList argv.(2) [] in
+  let dirpath = if argv.(3) = "" then "./FileFinderData" else argv.(3) in
   match args.(1) with
   | "export" ->     
     let results = total_res_dict "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/" in
@@ -15,13 +15,14 @@ let main () =
     let trash = dir_accumulate_merged results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data_merged/" cmdlist in
     ignore(trash)
   | "ami" -> 
-    let oc = open_out ("results/metadata.ami_results.txt")
-    (ignore(print_result_ami oc (ami_dict dirpath cmdlist));
+    let oc = open_out ("results/metadata.ami_results.txt") in
+    ignore(print_result_ami oc (ami_dict dirpath cmdlist));
     close_out oc;
     let oc = open_out ("results/ami_results.txt") in
-    ami_textify "results/metadata.ami_results.txt" oc)
+    ami_textify "results/metadata.ami_results.txt" oc
   | "surf" -> 
     let res, txtout = (surf_dict dirpath cmdlist, "surf_results.txt") in
+    let oc = open_out ("results/" ^ txtout) in
     ignore (print_result oc res)
   | "vox" | "libri" | "vy" | "wsj" -> 
     let res, txtout = (match argv.(1) with
@@ -31,18 +32,19 @@ let main () =
         true, "libri_results.txt")
     | "vy" -> (find_words cmdlist accesstext_vy accesswav_vy dirpath false, 
       "vy_results.txt")
-    | "wsj" -> print_endline "wsj"; (wsj0_dict dirpath cmdlist, "wsj_results.txt")) in
+    | "wsj" -> print_endline "wsj"; (wsj0_dict dirpath cmdlist, "wsj_results.txt")
+    | _ -> (D.empty,"")) in
     let cmd_dict = make_cmd_dict res D.empty in
     let oc = open_out ("results/" ^ txtout) in
-    print_result oc cmd_dict
-  | _ -> failwith (Invalid_argument "argv 1 is invalid")
+    ignore (print_result oc cmd_dict)
+  | _ -> failwith "argv 1 is invalid"
   ;;
 
 (* let main () = 
   let simpleton = fun x y z -> x in
   let args = Sys.argv in
   let cmdlist = getCmdList argv.(2) [] in
-  let dirpath = if argv.(3) = "" then "./FileFinderData" else argv.(2) in
+  let dirpath = if argv.(3) = "" then "./FileFinderData" else argv.(3) in
   if argv.(1) = "export" then 
     let results = total_res_dict "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/" in
     let trash = dir_accumulate results "/Users/justinkae/Documents/TensorFlowPractice/FileFinderFolder/results/data/" in
