ad2e95cfb9f6dd26b13f5c9d01db8e53f6f2734b
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-31 16:41:59 -0700

    need to track graph, default graph is empty for some reason

--- FileFinderFolder/PSF/VGG_display.py
+++ FileFinderFolder/PSF/VGG_display.py
@@ -5,8 +5,10 @@ vgg = VGG.VGG("./MFCCData_Folder/MFCCData.npz")
 # vgg_m = VGG.VGG("./MFCCData_merged.npz")
 vgg.build(tf.placeholder(tf.float32, shape=(965, 1568, 14, 1)))
 
+print tf.get_default_graph()
+
 # TensorBoard graph:
-with tf.Session() as sess:
-    writer = tf.summary.FileWriter('./summary')
-    writer.add_graph(tf.get_default_graph())
-    writer.close
+# with tf.Session() as sess:
+#     writer = tf.summary.FileWriter('./summary')
+#     writer.add_graph(tf.get_default_graph())
+#     writer.close
