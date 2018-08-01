4a62b6ce4e986ed39197e8776b34aed4232c44a7
EQUASHNZRKUL <justinkae@gmail.com>
2018-07-31 19:03:31 -0700

    VGG_train confirmed working

--- FileFinderFolder/PSF/VGG_train.py
+++ FileFinderFolder/PSF/VGG_train.py
@@ -1,32 +1,21 @@
 import VGG
 import tensorflow as tf
 
-vgg = VGG.VGG("./MFCCData_folder/MFCCData.npz")
-vgg.split({'train':6, 'test':4})
-# vgg_m = VGG.VGG("./MFCCData_folder/MFCCData_merged.npz")
 vgg_train = VGG.VGG("./MFCCData_folder/MFCCData_split/train.npz")
 
-def train():
-  with tf.Graph().as_default():
-    global_step = tf.train.get_or_create_global_step()
+with tf.Graph().as_default():
+  global_step = tf.train.get_or_create_global_step()
 
-    # TODO: Get data, labels
-    data, labels = vgg_train.dic_to_inputs(vgg_train.datadict)
+  vgg = VGG.VGG("./MFCCData_folder/MFCCData.npz")
+  vgg.split({'train':6, 'test':4})
+  data, labels = vgg.dic_to_inputs(vgg.datadict['train'])
 
-    # TODO: Pass data into [build]
-    # TODO: Calculate loss
-    # TODO: Execute train_step with loss and global_step and build new graph
-
-    logits = vgg_train.build(data)
-
-    loss = vgg_train.loss(logits, labels)
-
-    train_op = vgg_train.train(loss, global_step)
-    
-    # vgg_train.build(tf.placeholder(tf.float32, shape=(14, 1568, 1, 1)))
+  logits = vgg.build(data)
+  loss = vgg.loss(logits, labels)
+  train_op = vgg.train(loss, global_step)
 
 with tf.Session() as sess:
   writer = tf.summary.FileWriter('./summary')
-  writer.add_graph(tf.get_default_graph())
+  writer.add_graph(train_op.graph)
   writer.close
 
